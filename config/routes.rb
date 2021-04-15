cb223f27d88db0d8549fb5abed4af95feece8ac8
Ola Hola <homoncik.aleksandra@gmail.com>
2021-04-15 15:49:51 +0200

    lists & bookmars controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:new, :create, :index, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# lists     GET  /lists(.:format)        index
+#           POST /lists(.:format)        create
+# new_list  GET  /lists/new(.:format)    new
+# list      GET  /lists/:id(.:format)    show
+
+# list_bookmarks POST /lists/:list_id/bookmarks(.:format)   create
+# new_list_bookmark GET    /lists/:list_id/bookmarks/new    new
+# bookmark DELETE /bookmarks/:id(.:format)                  destroy
