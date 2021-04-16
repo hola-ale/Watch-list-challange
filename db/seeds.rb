05fd4dc10ef7ff5ad673ca8479dbec8a72ea8152
Ola Hola <homoncik.aleksandra@gmail.com>
2021-04-16 12:50:22 +0200

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,11 @@ Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s f
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-# puts "Creating movies..."
-# 10.times do
-#   movie = Movie.create(
-#     title: Faker::Movie.title,
-#     overview: Faker::Movie.quote,
-#     poster_url: "https://unsplash.com/s/photos/movie",
-#     rating: rand(1..10)
-#     )
-#   puts "Movie #{movie.id} is created"
-# end
-# puts "Finished!"
+10.times do
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: "https://unsplash.com/s/photos/movie",
+    rating: rand(1..10)
+    )
+end
